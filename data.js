6a80e0bd09721d395e27c6223fe4f73d6bf8e9e4
nazwa użytkownika <58075465+nazwa-uzytkownika@users.noreply.github.com>
2020-08-21 17:52:26 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano-360-zse",
+      "name": "Pano 360 ZSE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.5595952234908204,
+        "pitch": -0.023783974564501875,
+        "fov": 1.2656532013347397
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Panorama DJI Tello (15 m n.p.g.) - kielecki \"ELEKTRYK\" i okolice",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
